b8779467eeb6c2f37d92387f4f4a5462bb186850
wangxiaobai <1510106069@qq.com>
2021-04-19 11:21:45 +0800

    element-plus 按需引入

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ElButton } from 'element-plus'
 import router from '@/router'
+import 'element-plus/lib/theme-chalk/index.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(ElButton)
 app.mount('#app')
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,7 @@
 <template>
-  index
+  <div>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
 </template>
 
 <script>
